236688f92be1b5eb47671921e69b4c0000b61071
Roger <136284351+whoisrgxu@users.noreply.github.com>
2025-09-17 12:04:56 -0400

    Update read-databases.js

--- read-databases.js
+++ read-databases.js
@@ -1,14 +1,13 @@
-const { MongoClient } = require('mongodb');
+const { collection } = require('./mongo_db');
 const {sql} = require('./supa_db');
 (async () => {
-  // Connect to MongoDB
-  const client = new MongoClient(process.env.MONGO_URI);
-  await client.connect();
-  const collection = client.db('smart-cover-letter').collection('users');
-  const mongoData = await collection.find({}).toArray();
-  console.log('MongoDB Data:', mongoData);
-
-  // Connect to Supabase
+  try {
+    // Query to select all rows from your collection, e.g. 'users'
+    const mongoData = await collection.find({}).toArray();
+    console.log('MongoDB Data:', mongoData);
+  } catch (error) {
+    console.error('Error reading data:', error)
+  }
   try {
     // Query to select all rows from your table, e.g. 'users'
     const users = await sql`SELECT * FROM users;`
