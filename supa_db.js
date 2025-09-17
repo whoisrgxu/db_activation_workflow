c89baae7aa59a506acd92250f2c8a7be910a8737
Roger <136284351+whoisrgxu@users.noreply.github.com>
2025-09-17 12:32:02 -0400

    Update supa_db.js

--- supa_db.js
+++ supa_db.js
@@ -1,4 +1,4 @@
 const postgres = require('postgres');
 const connectionString = process.env.SUPABASE_CONNECTION_URL
 const sql = postgres(connectionString)
-export default sql
+module.exports = sql;
