774032f91c5cd9a27e1bbc03afb7b758adeb58ba
Roger <136284351+whoisrgxu@users.noreply.github.com>
2025-09-17 13:29:55 -0400

    Update read-databases.js

--- read-databases.js
+++ read-databases.js
@@ -1,21 +1,22 @@
-const { connectMongo, client }  = require('./mongo_db');
-const sql  = require('./supa_db');
+const { connectMongo, client } = require('./mongo_db');
+const supa = require('./supa_db');
 
 (async () => {
   try {
-    const collection = await connectMongo()
-    const mongoData = await collection.find({}).toArray()
-    console.log('MongoDB Data:', mongoData)
+    const collection = await connectMongo();
+    const mongoData = await collection.find({}).toArray();
+    console.log('MongoDB Data:', mongoData);
   } catch (error) {
-    console.error('MongoDB Error:', error)
+    console.error('MongoDB Error:', error);
   }
 
   try {
-    const holdings = await sql`SELECT * FROM holdings;`
-    console.log('Supabase Users:', holdings)
+    const holdings = await supa.query`SELECT * FROM holdings;`;
+    console.log('Supabase Users:', holdings);
   } catch (error) {
-    console.error('Supabase Error:', error)
+    console.error('Supabase Error:', error);
   } finally {
-    await client.close()
+    await client.close();     // close Mongo
+    await supa.close();       // close Postgres
   }
-})()
+})();
